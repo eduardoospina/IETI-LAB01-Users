d77a6f5b4fadffe967f7cc947522586b7d17cb21
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-08-28 10:17:26 -0500

    service package

---
+++ src/main/java/IETI/Lab1User/service/UserServiceHashMap.java
@@ -0,0 +1,38 @@
+package IETI.Lab1User.service;
+
+import IETI.Lab1User.entities.User;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@Service
+public class UserServiceHashMap implements UserService{
+
+    private final ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public User findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(String id) {
+
+    }
+
+    @Override
+    public User update(User user, String userId) {
+        return null;
+    }
+}
